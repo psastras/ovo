a6e7cb1677abfd6904c9b5e23bc0203e0542d38a
Paul Sastrasinh <psastras@gmail.com>
2016-12-13 00:13:34 -0500

    refactor: move home components to home directory

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import Search from '../components/search';
-import Spans from '../components/spans';
+import Search from './search';
+import Spans from './spans';
 
 interface HomeProps {
   location?: any;
