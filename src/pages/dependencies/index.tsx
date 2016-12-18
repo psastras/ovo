497d62dccbce7af90402bc10351bca4cf0499ef7
Paul Sastrasinh <psastras@gmail.com>
2016-12-18 18:22:57 -0500

    fix: fetch dependencies on page load

--- src/pages/dependencies/index.tsx
+++ src/pages/dependencies/index.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import Actions from 'src/flux/actions';
+import * as moment from 'moment';
 
 interface DependenciesProps {
   location?: any;
+  fetchDependencies?: (endTs: number) => void;
 }
 
 export class Dependencies extends React.Component<DependenciesProps, {}> {
 
-  public render() {
+  public componentWillMount(): void {
+    this.props.fetchDependencies(moment().valueOf());
+  }
+
+  public render(): JSX.Element {
     return (
       <div>
         <h1>Under Construction</h1>
@@ -22,4 +29,10 @@ const mapStateToProps = (state: {}, props: DependenciesProps): DependenciesProps
   };
 };
 
-export default connect(mapStateToProps)(Dependencies);
+const mapDispatchToProps = (dispatch): DependenciesProps => {
+  return {
+    fetchDependencies: (endTs: number) => dispatch(Actions.zipkin.getDependencies(endTs)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dependencies);
