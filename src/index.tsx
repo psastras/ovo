d6b892d5a3509330d7972be52224c12df9e53758
Paul Sastrasinh <psastras@gmail.com>
2016-12-18 14:28:43 -0500

    fix: line wrap
    broke the build

--- src/index.tsx
+++ src/index.tsx
@@ -48,7 +48,8 @@ ReactDOM.render(
         <Router history={history}>
           <Route path='/trace/:traceId'
             getComponent={() => System.import('./pages/trace').then(c => layout(c.default))} />
-          <Route path='/' getComponent={() => System.import('./pages/home').then(c => layout(c.default))} />
+          <Route path='/'
+            getComponent={() => System.import('./pages/home').then(c => layout(c.default))} />
         </Router>
       </div>
     </LocaleProvider>
